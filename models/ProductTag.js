9f504bbaf7af75a7529c65e7985143e8b990efff
hershRVA <herchenrother@gmail.com>
2023-10-23 17:21:43 -0400

    set models for database

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,30 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product', // This should match the model name in the association
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag', // This should match the model name in the association
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -4,13 +4,30 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Associations
+
+// Products belong to a Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id', // This should match the column name in the Product model
+  onDelete: 'CASCADE', // If a category is deleted, also delete the products in that category
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id', // This should match the column name in the Product model
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belong to many Tags through ProductTag
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id', // This should match the column name in the ProductTag model
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belong to many Products through ProductTag
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id', // This should match the column name in the ProductTag model
+});
 
 module.exports = {
   Product,
